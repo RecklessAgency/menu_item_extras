649eaebb9d82a246052c2f474a9212a15e8a71e7
Bogdan Hepting <bogdan.hepting@lemberg.co.uk>
2017-12-04 13:04:59 +0200

    Fix linter errors

--- modules/menu_item_extras_demo_base/webpack.bundle.config.js
+++ modules/menu_item_extras_demo_base/webpack.bundle.config.js
@@ -1,27 +1,23 @@
-'use strict';
-
-module.exports = (configs) => {
-  return {
-    output: {
-      filename: 'bundle.js',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /(node_modules)/,
-          loader: 'babel-loader',
-          query: {
-            presets: [
-              ["env", {
-                "targets": {
-                  "browsers": configs.browsersSupport
-                }
-              }]
-            ],
-          },
+module.exports = configs => ({
+  output: {
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: [
+            ['env', {
+              targets: {
+                browsers: configs.browsersSupport,
+              },
+            }],
+          ],
         },
-      ],
-    },
-  };
-};
+      },
+    ],
+  },
+});
--- modules/menu_item_extras_demo_base/webpack.standalone.config.js
+++ modules/menu_item_extras_demo_base/webpack.standalone.config.js
@@ -1,28 +1,24 @@
-'use strict';
-
-module.exports = (configs) => {
-  return {
-    entry: {},
-    output: {
-      filename: '[name]',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /(node_modules)/,
-          loader: 'babel-loader',
-          query: {
-            presets: [
-              ["env", {
-                "targets": {
-                  "browsers": configs.browsersSupport
-                }
-              }]
-            ],
-          },
+module.exports = configs => ({
+  entry: {},
+  output: {
+    filename: '[name]',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: [
+            ['env', {
+              targets: {
+                browsers: configs.browsersSupport,
+              },
+            }],
+          ],
         },
-      ],
-    },
-  };
-};
+      },
+    ],
+  },
+});
